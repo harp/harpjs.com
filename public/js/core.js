f63470b156cf42f161efc4fb582b119af813c02a
Kenneth Ormandy <kenneth@chloi.io>
2016-01-21 15:40:00 -0800

    Inlines core scripts

---
+++ public/js/core.js
@@ -0,0 +1,2 @@
+// var loadCSS = require('fg-loadcss') // This is inlined in _layout.jade
+loadCSS('http://fonts.googleapis.com/css?family=Work+Sans:300,400,700,800,900,600,500,200,100&subset=latin,latin-ext')
